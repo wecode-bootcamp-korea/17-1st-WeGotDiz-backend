b866cb6d09884d0b9c2281222c8a2f14d02c7315
mjhuh263 <mjhuh263@gmail.coom>
2021-02-17 21:37:49 +0900

    MODIFY: all apps models.py

--- purchase/models.py
+++ purchase/models.py
@@ -10,28 +10,23 @@ class Address(models.Model):
         db_table = 'addresses'
 
 class Order(models.Model):
-    order_number      = models.CharField(max_length=150)
-    fullname          = models.CharField(max_length=45)
+    fullname          = models.CharField(max_length=45) #POST FULL NAME
     contact_number    = models.CharField(max_length=20)
     delivery_note     = models.CharField(max_length=50, null=True)
-    card_number       = models.CharField(max_length=250) # 멘토님께 여쭤보기
-    card_password     = models.CharField(max_length=250)
-    card_expire       = models.CharField(max_length=5)
-    delivery_fee      = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_fee      = models.DecimalField(max_digits=10, decimal_places=2, default=2500) 
     donation          = models.DecimalField(max_digits=2, decimal_places=1, null=True)
-    date_of_birth     = models.DateTimeField()
     created_at        = models.DateTimeField(auto_now_add=True)
     address           = models.ForeignKey('Address', on_delete=models.PROTECT) #exists -> get or create
-    order_status      = models.CharField(max_length=45, default='배송준비중')
-    reward            = models.ManyToManyField('product.Reward', through='RewardOrder') # through_fields=('reward', 'order') 
+    order_status      = models.CharField(max_length=45, default='결제완료') # 이거 꼭 해야 함?
+    reward            = models.ManyToManyField('product.Reward', through='RewardOrder') 
     user              = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'orders'
 
 class RewardOrder(models.Model): # 펀딩하기 아이템/주문하기 - 수량 체크 펀딩
-    reward      = models.ForeignKey('product.Reward', on_delete=models.PROTECT) # 내일 생각하기
-    order       = models.ForeignKey('Order', on_delete=models.PROTECT) # 내일 생각하기
+    reward      = models.ForeignKey('product.Reward', on_delete=models.PROTECT) 
+    order       = models.ForeignKey('Order', on_delete=models.PROTECT) 
     quantity    = models.PositiveIntegerField(default=0)
 
     class Meta:
--- user/models.py
+++ user/models.py
@@ -2,7 +2,8 @@ from django.db        import models
 
 class User(models.Model):
     fullname    = models.CharField(max_length=20)
-    email       = models.EmailField()
+    image       = models.URLField(max_length=2000)
+    email       = models.EmailField(max_length=200)
     password    = models.CharField(max_length=200)
     event_code  = models.CharField(max_length=6)
     maker_info  = models.ForeignKey('MakerInfo', on_delete=models.CASCADE, null=True)
