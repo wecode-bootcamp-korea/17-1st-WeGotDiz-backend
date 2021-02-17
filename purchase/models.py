c2b90eb362679822e198b818798b90e1836fa39f
mjhuh263 <mjhuh263@gmail.coom>
2021-02-17 21:53:32 +0900

    MODIFY: all apps models.py

--- purchase/models.py
+++ purchase/models.py
@@ -4,27 +4,27 @@ from user.models    import User
 
 class Address(models.Model):
     address           = models.CharField(max_length=250)
-    user              = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True) # 회사 : 탈퇴한 회원의 배송 주소는 보여줘야 한다
+    user              = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True) 
 
     class Meta:
         db_table = 'addresses'
 
 class Order(models.Model):
-    fullname          = models.CharField(max_length=45) #POST FULL NAME
+    fullname          = models.CharField(max_length=45) 
     contact_number    = models.CharField(max_length=20)
     delivery_note     = models.CharField(max_length=50, null=True)
     delivery_fee      = models.DecimalField(max_digits=10, decimal_places=2, default=2500) 
     donation          = models.DecimalField(max_digits=2, decimal_places=1, null=True)
     created_at        = models.DateTimeField(auto_now_add=True)
-    address           = models.ForeignKey('Address', on_delete=models.PROTECT) #exists -> get or create
-    order_status      = models.CharField(max_length=45, default='결제완료') # 이거 꼭 해야 함?
+    address           = models.ForeignKey('Address', on_delete=models.PROTECT) 
+    order_status      = models.CharField(max_length=45, default='결제완료') 
     reward            = models.ManyToManyField('product.Reward', through='RewardOrder') 
     user              = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'orders'
 
-class RewardOrder(models.Model): # 펀딩하기 아이템/주문하기 - 수량 체크 펀딩
+class RewardOrder(models.Model):
     reward      = models.ForeignKey('product.Reward', on_delete=models.PROTECT) 
     order       = models.ForeignKey('Order', on_delete=models.PROTECT) 
     quantity    = models.PositiveIntegerField(default=0)
--- user/models.py
+++ user/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     image       = models.URLField(max_length=2000)
     email       = models.EmailField(max_length=200)
     password    = models.CharField(max_length=200)
-    event_code  = models.CharField(max_length=6)
     maker_info  = models.ForeignKey('MakerInfo', on_delete=models.CASCADE, null=True)
 
     class Meta:
