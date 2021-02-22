77b4959c1c35d69a130046a47ce58a95af9677da
mjhuh263 <mjhuh263@gmail.coom>
2021-02-23 06:15:05 +0900

    MODIFY: SignInView statuscode, nested if

--- user/views.py
+++ user/views.py
@@ -22,16 +22,16 @@ class SignInView(View):
             if not (email and password):
                 return JsonResponse({"message" : "REQUIRED_FIELD"}, status=400)
 
-            if User.objects.filter(email=email):
-                user = User.objects.get(email=email)
+            if not User.objects.filter(email=email):
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
 
-                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                    access_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm=ALGORITHM)
-                    return JsonResponse({"message" : "SUCCESS", "TOKEN" : access_token}, status=200)
+            user = User.objects.get(email=email)
+
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                access_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({"message" : "SUCCESS", "TOKEN" : access_token}, status=200)
                 
-                return JsonResponse({"message" : "UNAUTHORIZED_APPROACH"}, status=401)
-            
-            return JsonResponse({"message" : "INVALID_USER"}, status=400)
+            return JsonResponse({"message" : "UNAUTHORIZED_APPROACH"}, status=401)
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
