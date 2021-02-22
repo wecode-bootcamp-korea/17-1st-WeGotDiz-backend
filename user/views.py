de072c1f94002b1f29d67a69f8f0e96a35ecc7b1
mjhuh263 <mjhuh263@gmail.coom>
2021-02-23 06:17:34 +0900

    MODIFY: SignInView statuscode, nested if

--- user/views.py
+++ user/views.py
@@ -30,9 +30,8 @@ class SignInView(View):
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 access_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm=ALGORITHM)
                 return JsonResponse({"message" : "SUCCESS", "TOKEN" : access_token}, status=200)
-                
+
             return JsonResponse({"message" : "UNAUTHORIZED_APPROACH"}, status=401)
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-    
