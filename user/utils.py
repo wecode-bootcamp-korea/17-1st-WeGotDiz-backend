93c859c09bdf47617b6a2b7630e5724cdd4f7b05
mjhuh263 <mjhuh263@gmail.coom>
2021-02-23 13:57:06 +0900

    MODIFY: feature/signin MERGED FILES

--- user/utils.py
+++ user/utils.py
@@ -6,7 +6,7 @@ from django.http              import JsonResponse
 from django.conf              import settings
 
 from my_settings              import SECRET_KEY, ALGORITHM
-from .models                   import User
+from .models                  import User
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
