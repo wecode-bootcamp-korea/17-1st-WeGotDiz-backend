2d78071247893f3e95dffceb10dad244bd227a5f
mjhuh263 <mjhuh263@gmail.coom>
2021-02-23 10:39:30 +0900

    MODIFY: UserFundView

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import UserLikeView
+from user.views import UserLikeView, UserFundView
 
 urlpatterns = [
-   path('/list', UserLikeView.as_view())   # mypage로 바꿔라 나중에;
+   path('/likelist', UserLikeView.as_view()),   # mypage로 바꿔라 나중에;
+   path('/fundinglist', UserFundView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -25,6 +25,8 @@ class UserLikeView(View):
         
         user = User.objects.get(id=request.user.id)
         fullname = user.fullname
+
+        #TOTAL FUNDING< LIKES
             
         likes = LikeUser.objects.filter(user=request.user.id) 
 
@@ -38,7 +40,7 @@ class UserLikeView(View):
 
                     like_list.append(
                         {
-                            "user" : request.user.id,
+                            # "user" : request.user.id, 이게 꼭 필요항가?
                             "product_id" : like.product.id,
                             "product_title" : like.product.title,
                             "product_image" : like.product.thumbnail_url,
@@ -54,15 +56,38 @@ class UserLikeView(View):
 # http -v GET 127.0.0.1:8000/user/list "Authorization:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTF9.qUSca7Udz6mGqgfPdnrKAmldt76CiPFjrTU0g9c_qzQ"
 
 class UserFundView(View):
-    @login_decorator
+    # @login_decorator
     def get(self, request):
-        if not User.objects.filter(id=request.user.id):
+        if not User.objects.filter(id=2):
+        # if not User.objects.filter(id=request.user.id):
             return JsonResponse({"message" : "INVALID_USER"})
         
-        user = User.objects.get(id=request.user.id)
+        user = User.objects.get(id=2)
+        # user = User.objects.get(id=request.user.id)
         fullname = user.fullname
 
-        fundings = 
+        #TOTAL FUNDING< LIKES
+
+        funding_list = []
+
+        # fundings = Order.objects.filter(user=request.user.id).prefetch_related('reward')
+
+        fundings = Order.objects.filter(user=2).prefetch_related('reward')
+        fundings = fundings.prefetch_related('product')
+
+        for funding in fundings:
+            funding_list.append(
+                {
+                    "fullname" : funding.fullname,
+                    "product_title": funding.title 
+                }
+            )
+
+        # fundings = 
+
+        return JsonResponse({"fullname" : fullname, "result" : funding_list}, status=200)
+
+
         
 
 
