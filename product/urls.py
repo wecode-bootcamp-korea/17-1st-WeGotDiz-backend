e85daf505550c3b051e9d088f67cee6cd8a30e10
이동근 <eagle5424228@github.com>
2021-02-24 19:20:07 +0900

    MODIFY: 필터링 제외한 나머지 부분push

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from .views      import ProductListView, ProductlistQueryView
-from .views      import ProductListView
+from .views      import ProductListView 
 
 urlpatterns = [
     path('/main', ProductListView.as_view()),    
     path('/main/<int:category_id>', ProductListView.as_view()),
-    path('/main/query', ProductlistQueryView.as_view())
 ]
--- product/views.py
+++ product/views.py
@@ -5,6 +5,7 @@ import jwt
 from datetime                 import date, datetime
 from django.http              import HttpResponse, JsonResponse
 from django.views             import View
+from django.db.models         import Q
 
 from .models                  import (
         Category, 
@@ -41,41 +42,4 @@ class ProductListView(View):
         
         return JsonResponse({"MESSAGE" : "SUCCESS", "DATA" : product_list}, status=200)
 
-
-# 쿼리문으로 코드 줄이기
-class ProductlistQueryView(View):
-    def get(self, request):
-        data  = request.GET.get('category', 0)
-        endYN = request.GET.get('endYN', 1)
-        #order = request.GET.get('order', 'recommend')
-        today = datetime.today()
-        
-        products = Product.objects.filter() if data ==0\
-                else Product.objects.filter(category__id=data)
-
-        category = Category.objects.filter().first() if data == 0\
-                else Category.objects.filter(product__id=data).first()
-
-        closing_date = (products.first().closing_date - datetime.today()).days
-
-        products = products.filter(closing_date__lt) if endYN == 2 else products.filter(closing_date < 10)
-
-
-        
-        product_list = [{
-            'category_image'   : category.image,
-            'title'            : product.title,
-            'goal_amout'       : product.goal_amount,
-            'toal_amount'      : product.total_amount,
-            'achieved_rate'    : product.achieved_rate,
-            'total_supporters' : product.total_supporters,
-            'closing_date'     : str((product.closing_date - today).days),
-            'thumbnail'        : product.thumbnail_url,
-            'category'         : category.name,
-            'category_id'      : category.id,
-            'id'               : product.id,
-            'maker_info_name'  : product.maker_info.name
-            } for product in products]
-        
-        return JsonResponse({"MESSAGE" : "SUCCESS", "DATA" : product_list}, status=200)
 
