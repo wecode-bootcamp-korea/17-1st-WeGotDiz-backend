546aeb24fbe7cc41a96ff89c3226a4b4a93f84ef
mjhuh263 <mjhuh263@gmail.coom>
2021-02-24 17:19:37 +0900

    MODIFY UserFundView, UserLikeView list comprehension

--- user/views.py
+++ user/views.py
@@ -20,37 +20,27 @@ from my_settings                       import SECRET_KEY, ALGORITHM
 class UserLikeView(View):
     @login_decorator
     def get(self, request):
-
         if not User.objects.filter(id=request.user.id):
             return JsonResponse({"message" : "INVALID_USER"})
         
         user = User.objects.get(id=request.user.id)
                     
-        likes = LikeUser.objects.filter(user=request.user.id) 
-
-        data = []
-
-        for like in likes:
-                all_categories = like.product.category_set.all()
-
-                for category in all_categories: 
-                    each_category = category.name
-                    # 한번에 드리기 -> data + list 까먹으면 죽음뿐... 
-                    data = [
-                                {
-                                    "id"                    : user.id,
-                                    "fullname"              : user.fullname,
-                                    "product_id"            : like.product.id,
-                                    "product_title"         : like.product.title,
-                                    "product_image"         : like.product.thumbnail_url,
-                                    "product_maker_info"    : like.product.maker_info.name,
-                                    "product_total_amount"  : like.product.total_amount,  
-                                    "product_achieved_rate" : like.product.achieved_rate, 
-                                    "product_category"      : each_category
-                                }
-                            ]
+        likes = LikeUser.objects.filter(user=request.user.id)
         
-        return JsonResponse({"data" : data}, status=200)
+        data = [{
+            "id"                    : user.id,
+            "fullname"              : user.fullname,
+            "product_id"            : like.product.id,
+            "product_title"         : like.product.title,
+            "product_image"         : like.product.thumbnail_url,
+            "product_maker_info"    : like.product.maker_info.name,
+            "product_total_amount"  : like.product.total_amount,  
+            "product_achieved_rate" : like.product.achieved_rate, 
+            "product_category"      : [category.name for category in like.product.category_set.all()]
+
+        } for like in likes]
+        
+        return JsonResponse({"mypage_data" : data}, status=200)
 
 class UserFundView(View):
     @login_decorator
@@ -59,28 +49,24 @@ class UserFundView(View):
             return JsonResponse({"message" : "INVALID_USER"})
         
         user = User.objects.get(id=request.user.id)
+        
+        data = [
+            
+            {
+                "id"                     : user.id,
+                "fullname"               : user.fullname,
+                "product_image"          : reward.product.thumbnail_url,
+                "product_date_countdown" : str((datetime.datetime.today() - reward.product.closing_date).days),
+                "product_total_amount"   : reward.product.total_amount,
+                "producst_achieved_rate"  : reward.product.achieved_rate,
+                "product_title"          : reward.product.title,
+                "product_maker_info"     : reward.product.maker_info.name,
+                "product_category"       : [category.name for category in reward.product.category_set.all()]
+            }
+            for order in user.order_set.all()
+            for reward in order.reward.all()]
 
-        data = []
-
-        for order in user.order_set.all():
-            for reward in order.reward.all():
-                closing_date = datetime.datetime.today() - reward.product.closing_date
-
-                data = [
-                    {
-                        "id"                     : user.id,
-                        "fullname"               : user.fullname,
-                        "product_image"          : reward.product.thumbnail_url,
-                        "product_date_countdown" : closing_date.days,
-                        "product_total_amount"   : reward.product.total_amount,
-                        "product_achieved_rate"  : reward.product.achieved_rate,
-                        "product_title"          : reward.product.title,
-                        "product_maker_info"     : reward.product.maker_info.name,
-                        "product_category"       : reward.product.category_set.all()[0].name,
-                    }
-                ]
-
-                return JsonResponse({"data" : data}, status=200)
+        return JsonResponse({"mypage_data" : data}, status=200)
 
 class UserInfoView(View):
     @login_decorator  
